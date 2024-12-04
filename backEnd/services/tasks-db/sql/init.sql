8687279ca13f9e1badf0fff019e80c6c980e3dd6
Oleksandra Kuksa <83078383+lodgev@users.noreply.github.com>
2024-12-04 11:28:11 +0100

    Delete backEnd/services directory

--- backEnd/services/tasks-db/sql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS my_table (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    value INTEGER
-);
-INSERT INTO my_table (name, value)
-VALUES ('Alice', 42),
-    ('Bob', 36),
-    ('Charlie', 28);
