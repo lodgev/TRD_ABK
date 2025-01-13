8d0d4da31f550897cea8bbe066fc29174f3f8a31
Oleksandra <lodgev1@gmail.com>
2025-01-13 14:57:38 +0100

    [ADD] match-db

--- backEnd/services/match-db/sql/1-schema.sql
+++ backEnd/services/match-db/sql/1-schema.sql
@@ -1,13 +1,10 @@
--- public.matchs definition
--- Drop table
--- DROP TABLE public.match;
-CREATE TABLE public.match (
-    id uuid NOT NULL,
-    homeTeam varchar NOT NULL,
-    homeScore int NOT NULL,
-    awayTeam varchar NOT NULL,
-    awayScore int NULL,
-    scoreString varchar NULL,
-    matchDate time NULL,
-    CONSTRAINT matchs_pk PRIMARY KEY (id)
+CREATE TABLE clubs (
+    id SERIAL PRIMARY KEY,
+    rank INTEGER NOT NULL,
+    club VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    level VARCHAR(50) NOT NULL,
+    elo INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
 );
