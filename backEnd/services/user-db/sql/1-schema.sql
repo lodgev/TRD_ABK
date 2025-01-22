c8f62afc5d63571e754999756fd3f82a24958308
Oleksandra <lodgev1@gmail.com>
2025-01-22 11:45:36 +0100

    [user-db] user-stat table

--- backEnd/services/user-db/sql/1-schema.sql
+++ backEnd/services/user-db/sql/1-schema.sql
@@ -17,3 +17,11 @@ CREATE TABLE public.users (
     CONSTRAINT users_pk PRIMARY KEY (id),
     CONSTRAINT users_unique UNIQUE (email)
 );
+
+CREATE TABLE user_stats (
+    id SERIAL PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES public.users(id) ON DELETE CASCADE,
+    club_id INTEGER NOT NULL REFERENCES clubs(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(user_id, club_id) -- Користувач може лайкнути клуб тільки один раз
+);
