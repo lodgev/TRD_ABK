651a2d179cd6c1507a3c8e641ef1732b69609402
Doriane974 <61055322+Doriane974@users.noreply.github.com>
2025-01-24 15:30:43 +0100

    Update 2-data.sql

--- backEnd/services/recommender-db/sql/2-data.sql
+++ backEnd/services/recommender-db/sql/2-data.sql
@@ -6,7 +6,6 @@
 -- INSERT INTO football_transfers VALUES (5, 'Oumar Solet', 866708, 'NULL', '2024-09-15T09:55:43Z', 'Salzburg', 10013, 'Free agent', 2, NULL, 'NULL', False, False, 16000000);
 -- INSERT INTO football_transfers VALUES (6, 'Roland Sallai', 583601, 'NULL', '2024-09-14T07:15:03Z', 'Freiburg', 8358, 'Galatasaray', 8637, NULL, 'NULL', False, False, 17000000);
 INSERT INTO user_actions (club_id, user_id, action, timestamp) VALUES
--- User likes clubs
 (1, 'f928c455-d2f3-4e30-bf58-178ae041e8c2', 'liked', '2025-01-23 10:15:00'),
 (2, 'f928c455-d2f3-4e30-bf58-178ae041e8c2', 'disliked', '2025-01-23 10:20:00'),
 (3, 'f928c455-d2f3-4e30-bf58-178ae041e8c2', 'liked', '2025-01-23 10:25:00');
