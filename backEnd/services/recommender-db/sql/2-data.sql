cf21c7d8e691c26e135f1a92364113c98d63b96d
Oleksandra <lodgev1@gmail.com>
2025-01-25 20:17:19 +0100

    [changes] not working

--- backEnd/services/recommender-db/sql/2-data.sql
+++ backEnd/services/recommender-db/sql/2-data.sql
@@ -1,10 +1,3 @@
--- -- football transfers
--- INSERT INTO football_transfers VALUES (1, 'Kevin Denkey', 820477, 'NULL', '2024-11-22T01:17:59Z', 'Cercle Brugge', 9984, 'Cincinnati', 722265, NULL, 'NULL', False, False, 16000000);
--- INSERT INTO football_transfers VALUES (2, 'Paul Pogba', 248453, 'NULL', '2024-11-15T20:08:04Z', 'Juventus', 9885, 'Free agent', 2, NULL, 'NULL', False, False, 16000000);
--- INSERT INTO football_transfers VALUES (3, 'Raphaël Varane', 230982, 'NULL', '2024-09-25T10:40:11Z', 'Como', 10171, 'Free agent', 2, NULL, 'NULL', False, False, 15000000);
--- INSERT INTO football_transfers VALUES (4, 'Adrien Rabiot', 352879, 'NULL', '2024-09-17T19:17:18Z', 'Free agent', 2, 'Marseille', 8592, NULL, 'NULL', False, False, 33000000);
--- INSERT INTO football_transfers VALUES (5, 'Oumar Solet', 866708, 'NULL', '2024-09-15T09:55:43Z', 'Salzburg', 10013, 'Free agent', 2, NULL, 'NULL', False, False, 16000000);
--- INSERT INTO football_transfers VALUES (6, 'Roland Sallai', 583601, 'NULL', '2024-09-14T07:15:03Z', 'Freiburg', 8358, 'Galatasaray', 8637, NULL, 'NULL', False, False, 17000000);
 INSERT INTO user_actions (club_id, user_id, action, timestamp) VALUES
 -- User likes clubs
 (1, 'f928c455-d2f3-4e30-bf58-178ae041e8c2', 'liked', '2025-01-23 10:15:00'),
